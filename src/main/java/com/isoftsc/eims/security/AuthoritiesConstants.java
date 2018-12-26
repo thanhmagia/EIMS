7dfe262ca4d26c3c862b75db1c6bf27cf6c88999
Vuong Chi Quang <quang.vuong@formos.com>
2018-12-26 11:32:53 +0700

    First Commit

--- src/main/java/com/isoftsc/eims/security/AuthoritiesConstants.java
+++ src/main/java/com/isoftsc/eims/security/AuthoritiesConstants.java
@@ -6,9 +6,8 @@ package com.isoftsc.eims.security;
 public final class AuthoritiesConstants {
 
     public static final String ADMIN = "ROLE_ADMIN";
-
+    public static final String ACCOUNT = "ROLE_ACCOUNT";
     public static final String USER = "ROLE_USER";
-
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {
